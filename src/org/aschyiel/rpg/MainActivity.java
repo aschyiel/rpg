2aaf59e4ab9703376ce84ed3e334ab0fdf74a1d5
Ulysses Levy <levy.ulysses@gmail.com>
2013-12-17 22:51:20 -0800

    completed step2 of tutorial.

--- src/org/aschyiel/rpg/MainActivity.java
+++ src/org/aschyiel/rpg/MainActivity.java
@@ -1,17 +1,47 @@
 package org.aschyiel.rpg;
 
-import android.os.Bundle;
+import android.os.Bundle; 
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import org.andengine.engine.FixedStepEngine;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends BaseGameActivity {
+
+  //-----------------------------------
+  //
+  // Constants.
+  //
+  //-----------------------------------
+
+  private static final int CAMERA_WIDTH  = 800;
+  private static final int CAMERA_HEIGHT = 400;
+
+  //-----------------------------------
+  //
+  // Private Variables.
+  //
+  //-----------------------------------
+
+  private Camera camera;
+  
+  //-----------------------------------
+  //
+  // Public Methods
+  //
+  //-----------------------------------
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +49,40 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+  @Override
+  public EngineOptions onCreateEngineOptions() {
+    camera = new Camera( 0, 0, CAMERA_WIDTH, CAMERA_HEIGHT );
+    EngineOptions engineOptions = new EngineOptions( true,
+        ScreenOrientation.LANDSCAPE_FIXED,
+        new RatioResolutionPolicy( CAMERA_WIDTH, CAMERA_HEIGHT ),
+        camera );
+    return engineOptions;
+  }
+
+  @Override
+  public void onCreateResources(
+      OnCreateResourcesCallback pOnCreateResourcesCallback)
+      throws Exception {
+    // TODO Auto-generated method stub
+  	
+  }
+
+
+  @Override
+  public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
+      throws Exception {
+    // TODO Auto-generated method stub
+  	
+  }
+
+
+  @Override
+  public void onPopulateScene(Scene pScene,
+      OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+    // TODO Auto-generated method stub
+  	
+  }
     
 }
